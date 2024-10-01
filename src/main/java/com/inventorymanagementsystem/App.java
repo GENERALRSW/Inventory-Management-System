16e2e23cdc319408dd776d0980f82a735e742e47
tywaine <peterstywaine@gmail.com>
2024-09-30 20:52:17 -0500

    ...

--- src/main/java/com/inventorymanagementsystem/App.java
+++ src/main/java/com/inventorymanagementsystem/App.java
@@ -12,12 +12,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        PurchaseOrdersController.sendEmail(
-                "peterstywaine@gmail.com",
-                "mpap tyra nubl euln",
-                "peterstywaine@gmail.com",
-                "Test Email",
-                "JavaFx Test Email Number #2 \n Testing Testing");
         Model.getInstance();
         Model.getInstance().getViewFactory().dataBaseConnectionWindow();
     }
