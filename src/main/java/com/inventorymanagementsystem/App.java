82ca6ca79f1c088f5d724d0d93f4a22584591189
tywaine <peterstywaine@gmail.com>
2024-09-30 20:59:08 -0500

    ...

--- src/main/java/com/inventorymanagementsystem/App.java
+++ src/main/java/com/inventorymanagementsystem/App.java
@@ -12,7 +12,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Model.getInstance();
         Model.getInstance().getViewFactory().dataBaseConnectionWindow();
     }
 }
