15bc617f07f22bab33be44ba8393bd82989f9c84
tywaine <peterstywaine@gmail.com>
2024-11-16 19:55:51 -0500

    Deleted some methods

--- src/main/java/com/inventorymanagementsystem/Models/DataBaseDriver.java
+++ src/main/java/com/inventorymanagementsystem/Models/DataBaseDriver.java
@@ -1,25 +1,12 @@
 package com.inventorymanagementsystem.Models;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseDriver {
     private Connection connection;
 
     public DataBaseDriver(){}
-
-    public DataBaseDriver(Connection connection){
-        this.connection = connection;
-    }
-
-    public DataBaseDriver(String url, String user, String password){
-        try{
-            this.connection = DriverManager.getConnection(url, user, password);
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-    }
 
     public Connection getConnection() {
         return connection;
@@ -29,25 +16,12 @@ public class DataBaseDriver {
         this.connection = connection;
     }
 
-    public void setConnection(String url, String user, String password){
-        try{
-            this.connection = DriverManager.getConnection(url, user, password);
-        }catch(SQLException e){
-            e.printStackTrace();
-            System.out.println("Connection to database failed!!!");
-        }
-    }
-
     public void closeConnection(){
         try{
             connection.close();
         }catch (SQLException e){
             e.printStackTrace();
         }
-    }
-
-    public boolean connectionIsNull(){
-        return connection == null;
     }
 
     public boolean connectionIsNotNull(){
